ce51ef969e684862587a8e89fc6631af1c7b1c9d
KaiVerleysenPXL <kai.verleysen@student.pxl.be>
2025-11-19 20:08:29 +0100

    more debug for api url

--- VueFrontend/src/util/api.js
+++ VueFrontend/src/util/api.js
@@ -2,10 +2,11 @@ import { useCredentialsStore } from '@/stores/credentials';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // TODO use env for local + prod options
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use(async (config) => {
+  console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
   const credentialsStore = useCredentialsStore();
   config.headers['username'] = credentialsStore.username;
   config.headers['password'] = credentialsStore.password;
